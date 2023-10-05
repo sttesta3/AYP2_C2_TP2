7b0c9115b2f9c5708d6494c77339e228fa26c492
stesta <sttesta@fi.uba.ar>
2023-10-05 19:01:57 -0300

    cola codeada

--- include/cola.hpp
+++ include/cola.hpp
@@ -68,7 +68,54 @@ cola<T>::cola(){
 
 template <typename T>
 void cola<T>::alta(T dato){
-    
+    nodo<T> nuevo = new nodo(dato);
+
+    if (!this->primero())
+        this->primer_nodo = nuevo;
+    else
+        this->ultimo_nodo->cambiar_siguiente(nuevo);
+
+    this->ultimo_nodo = nuevo;
+    this->cantidad_datos++;
 }
+
+template <typename T>
+T cola<T>::baja(){
+    if (this->cantidad_datos == 0)
+        throw cola_exception;
+
+    // Guardamos nodo siguiente y resultado
+    nodo<T>* siguiente = this->primer_nodo->obtener_siguiente();
+    T resultado = this->primer_nodo->obtener_dato();
+
+    // Eliminamos primero, y el siguiente pasa a ser primero
+    delete this->primer_nodo;
+    this->primer_nodo = siguiente;
+    this->cantidad_datos--;
+
+    return resultado;
+}
+template <typename T>
+T cola<T>::primero(){
+    return this->primer_nodo->obtener_dato();
+}
+
+template <typename T>
+T cola<T>::ultimo(){
+    return this->ultimo_nodo->obtener_dato();
+}
+
+template <typename T>
+size_t cola<T>::tamanio(){
+    return this->cantidad_datos;
+}
+
+template <typename T>
+bool cola<T>::vacio(){
+    return (this->cantidad_datos == 0);
+}
+
+template <typename T>
+bool cola<T>::~cola(){}
 
 #endif
